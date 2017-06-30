b364b65794933879d2e7b92f4ec59c44c9ea1b3f
QKDevOpsGitHub <Quark@123>
2017-06-30 11:36:01 +0530

    8.2

--- app/src/main/java/com/qk/alertdialogtest/MainActivity.java
+++ app/src/main/java/com/qk/alertdialogtest/MainActivity.java
@@ -12,7 +12,7 @@ import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
     private Button mainBtn;
-//testing file
+//testing
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
